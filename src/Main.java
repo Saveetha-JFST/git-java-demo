bb19501fbaf6fa2581943d757f26ec17f9514572
sethu-ravichandran <rsethu1997@gmail.com>
2023-08-17 15:49:37 +0530

    Update - Made the adder() parameters as user-inputs.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(adder(4,5));
-        System.out.println(adder(8,9));
+        Scanner scanner = new Scanner(System.in);
+        int input1 = Integer.parseInt(scanner.nextLine());
+        int input2 = Integer.parseInt(scanner.nextLine());
+        System.out.println(adder(input1, input2));
     }
 
     static int adder(int number1, int number2)
