45f7b36c0b1c6312ae5abf6a93e20916a9b56e97
sethu-ravichandran <rsethu1997@gmail.com>
2023-08-17 15:46:58 +0530

    Add - Included the logic for adder() method.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println(adder(4,5));
+        System.out.println(adder(8,9));
+    }
+
+    static int adder(int number1, int number2)
+    {
+        return number1 + number2;
     }
 }
